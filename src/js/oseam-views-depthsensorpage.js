c1e7097e0d9583e3c2329acbbb348218b7c0a630
cleanerx <cleanerx1@googlemail.com>
2013-12-06 22:10:41 +0100

    F#https://github.com/OpenSeaMap/depth/issues/19 : Provide feedback for sensor positions

--- src/js/oseam-views-depthsensorpage.js
+++ src/js/oseam-views-depthsensorpage.js
@@ -13,13 +13,43 @@
 
 OSeaM.views.depthsensorpage = OSeaM.View.extend({
     modalDialog:null,
+    initialize : function() {
+        var depthoffset = this.model.get('depthoffset');
+        this.listenTo(depthoffset, 'change', this.render);
+    },
+    events : {
+    	"mousedown #draggableSounder": "dragStartEvent",
+    	"mouseup #vesselsize_depth": "dragStopEvent",
+    	"dragver #vesselsize_depth" : "dragOver"
+//    	"change #gps_distanceFromStern" : "changeY",
+//       	"change #gps_distanceFromCenter" : "changeX"
+    },
+    dragOver : function(e) {
+        e.preventDefault(); 
+        return false; 
+    },
+    dragStartEvent : function(e) {
+    	position =  new Object();
+    	position.x = e.clientX - parseInt($("#draggableSounder").css("left"),10);
+    	position.y = e.clientY - parseInt($("#draggableSounder").css("top"), 10);
+    },
+    dragStopEvent : function(e) {
+//    	$("#draggableGps").css("left", (e.clientX - position.x) + "px");
+//    	$("#draggableGps").css("top", (e.clientY - position.y) + "px");
+    },
     render: function() {
         var template = OSeaM.loadTemplate('depthsensoroffset');
         var depthoffset = this.model.get('depthoffset');
+        var relativeY = depthoffset.get('distanceFromStern') / this.model.get('loa');
+        var relativeX = depthoffset.get('distanceFromCenter') / this.model.get('breadth');
+        var topY = (((-1) * (relativeY * 250)) + 250) - 10 + "px";
+        var leftX = (((relativeX * 150)) + 75) - 10 + "px";
         var content = $(template({
         	depth_distanceFromStern : depthoffset.get('distanceFromStern'),
         	depth_distanceFromCenter : depthoffset.get('distanceFromCenter'),
-        	depth_distanceWaterline : depthoffset.get('distanceWaterline')
+        	depth_distanceWaterline : depthoffset.get('distanceWaterline'),
+        	top : topY,		
+        	left : leftX
 		}));
         OSeaM.frontend.translate(content);
         this.$el.html(content);
