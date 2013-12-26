4336a0a2022f69c891fded53e5ac230186632b6d
cleanerx <cleanerx@hotmail.de>
2013-12-26 19:07:45 +0100

    F: add gauges model

---
+++ src/js/oseam-models-gauges.js
@@ -0,0 +1,17 @@
+// -------------------------------------------------------------------------------------------------
+// OpenSeaMap Water Depth - Web frontend for depth data handling.
+//
+// Written in 2012 by Dominik Fässler dfa@bezono.org
+//
+// To the extent possible under law, the author(s) have dedicated all copyright
+// and related and neighboring rights to this software to the public domain
+// worldwide. This software is distributed without any warranty.
+//
+// You should have received a copy of the CC0 Public Domain Dedication along
+// with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+// -------------------------------------------------------------------------------------------------
+
+OSeaM.models.Gauges = Backbone.Collection.extend({
+    model: OSeaM.models.Gauge,
+    url: OSeaM.apiUrl + 'gauge'
+});
