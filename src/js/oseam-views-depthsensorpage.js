8d06ea3f182a20a258869b68513acb1abb471bf3
cleanerx <cleanerx1@googlemail.com>
2015-06-24 20:54:11 +0200

    Fix english handlebar
    https://github.com/OpenSeaMap/depth_webfrontend/issues/113

--- src/js/oseam-views-depthsensorpage.js
+++ src/js/oseam-views-depthsensorpage.js
@@ -67,7 +67,7 @@ OSeaM.views.depthsensorpage = OSeaM.View.extend({
 			depth_offsetKeel : depthoffset.get('offsetKeel'),
 			depth_manufacturer : depthoffset.get('manufacturer'),
 			depth_model : depthoffset.get('model'),
-			depthmeasurement : depthoffset.get('offsetType'),
+			depth_offsetType : depthoffset.get('offsetType'),
         	top : topY,		
         	left : leftX
 		}));
